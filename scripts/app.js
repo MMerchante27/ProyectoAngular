265620d73c0adeeceb260288043a5ba150fd7ca6
Miriam Merchante <miriam.merchante@babel.es>
2016-03-30 11:52:20 +0200

    Index, app.js, servicio URL y rutas.

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+// Defino el módulo "moviedb"
+angular.module("pelisBabel", ["ngRoute", "URL"]).config(
+    ["$routeProvider", "paths", function($routeProvider, paths) {
+        // Configuro las URLs de la aplicación
+        $routeProvider.when(paths.login, {
+            templateUrl: 'views/Login.html'
+        }).when(paths.newMovie, {
+            templateUrl: 'views/NewMovie.html',
+        }).when(paths.movieDetail, {
+            controller: 'MovieDetailController',
+            templateUrl: 'views/MediaItemDetail.html',
+        }).when(paths.home, {
+            redirectTo: paths.login
+        }).otherwise({
+            templateUrl: 'views/404.html'
+        })
+    }]
+);
---
+++ scripts/values/paths.js
@@ -0,0 +1,8 @@
+angular.module("pelisBabel").constant("paths", {
+    home:"/",
+    login: "/login",
+    movies: "/movies",
+    newMovie:"/movies/new",
+    movieDetail: "/movies/:id",
+    notFound: "/sorry"
+});
